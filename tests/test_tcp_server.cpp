8b17ca05216b3a438b45b647b1e8abd708805536
lu <1769982399@qq.com>
2023-03-02 21:36:44 +0800

    修复客户端主动断开服务器崩溃的问题

--- tests/test_tcp_server.cpp
+++ tests/test_tcp_server.cpp
@@ -17,16 +17,31 @@ class EchoServer : public fleet::TCPServer {
   void handle_client(fleet::Socket::Ptr client) override {
     InfoL << "new client: " << client->to_string();
     char buf[4096];  // 不用static，因为可能会有多线程竞争，不安全
-    memset(buf, 0, sizeof(buf));
-    int n = client->recv(buf, sizeof(buf));
-    InfoL << "recv: " << buf;
-    client->send(buf, n);
-    client->close();
+    while (true) {
+      memset(buf, 0, sizeof(buf));
+      int n = client->recv(buf, sizeof(buf));
+      if (n == -1) {
+        // 超时
+        InfoL << "recv timeout: " << client->to_string();
+        client->send("bye", 4);
+        client->close();
+        break;
+      } else if (n == 0) {
+        // 客户端断开了
+        InfoL << "client disconnect: " << client->to_string();
+        client->close();
+        break;
+      } else {
+        // 正常收到客户端的消息
+        InfoL << "recv: " << buf;
+        client->send(buf, n);
+      }
+    }
   }
 };
 
 void run() {
-  fleet::TCPServer::Ptr server(new EchoServer(3000));
+  fleet::TCPServer::Ptr server(new EchoServer(1000 * 6000));
   auto addr = fleet::Address::lookup_any_IPAddress("0.0.0.0:1234");
   ASSERT(addr);
 
@@ -42,12 +57,10 @@ void run() {
 int main() {
   LOG_DEFAULT;
   fleet::Logger::Instance().set_async();
-  fleet::Logger::Instance().set_level(fleet::LogLevel::Info);
+  // fleet::Logger::Instance().set_level(fleet::LogLevel::Info);
 
   fleet::IOManager iom(1, true);
   iom.schedule(run);
-
-  iom.stop();
 
   return 0;
 }
