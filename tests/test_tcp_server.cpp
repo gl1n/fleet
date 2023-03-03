f0709c2c43f420c8cc4d3b7ec679d0a7fd8b5c27
lu <1769982399@qq.com>
2023-03-03 10:43:38 +0800

    修复Windows主动断开连接导致ECONNREST的问题

--- tests/test_tcp_server.cpp
+++ tests/test_tcp_server.cpp
@@ -1,3 +1,4 @@
+#include <asm-generic/errno.h>
 #include <unistd.h>
 #include <cstdint>
 #include <cstring>
@@ -20,28 +21,29 @@ class EchoServer : public fleet::TCPServer {
     while (true) {
       memset(buf, 0, sizeof(buf));
       int n = client->recv(buf, sizeof(buf));
-      if (n == -1) {
+      if (n > 0) {
+        // 正常收到客户端的消息
+        InfoL << "recv: " << buf;
+        client->send(buf, n);
+      }
+      if (n == -1 && errno != ECONNRESET) {
         // 超时
         InfoL << "recv timeout: " << client->to_string();
         client->send("bye", 4);
         client->close();
         break;
-      } else if (n == 0) {
+      } else {
         // 客户端断开了
         InfoL << "client disconnect: " << client->to_string();
         client->close();
         break;
-      } else {
-        // 正常收到客户端的消息
-        InfoL << "recv: " << buf;
-        client->send(buf, n);
       }
     }
   }
 };
 
 void run() {
-  fleet::TCPServer::Ptr server(new EchoServer(1000 * 6000));
+  fleet::TCPServer::Ptr server(new EchoServer(1000 * 1));
   auto addr = fleet::Address::lookup_any_IPAddress("0.0.0.0:1234");
   ASSERT(addr);
 
