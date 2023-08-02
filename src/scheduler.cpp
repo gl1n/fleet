7290dcd075a11e6aeb125bc90aa5f9008575321c
lu <1769982399@qq.com>
2023-08-02 23:37:33 +0800

    提高代码可读性

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -90,28 +90,28 @@ void Scheduler::run() {
   while (true) {
     bool notify_me = false;
 
-    Task::Ptr ft = nullptr;
+    Task::Ptr task = nullptr;
 
     {
       MutexType::Lock lock(_task_mutex);
       auto it = _tasks.begin();
       while (it != _tasks.end()) {
-        if ((*it)->thread_id != -1 && (*it)->thread_id != fleet::get_thread_id()) {
+        task = *it;
+        if (task->thread_id != -1 && task->thread_id != fleet::get_thread_id()) {
           // 如果指定了线程而且指定的线程不是此线程
           ++it;
           notify_me = true;  // 没遍历完
           continue;
         }
 
-        ASSERT((*it)->fiber || (*it)->cb);  // fiber和cb至少得有一个
+        ASSERT(task->fiber || task->cb);  // fiber和cb至少得有一个
 
-        if ((*it)->fiber && (*it)->fiber->get_state() == Fiber::RUNNING) {
-          // 这里不太理解
+        if (task->fiber && task->fiber->get_state() == Fiber::RUNNING) {
           ++it;
           continue;
         }
 
-        ft = *it;
+        task = *it;
         it = _tasks.erase(it);   // 从队列中删除此任务
         ++_active_thread_count;  // 线程进入活跃状态
         break;
@@ -122,25 +122,25 @@ void Scheduler::run() {
       notify();
     }
 
-    if (ft) {           // 拿到任务
-      if (ft->fiber) {  // 是fiber
-        if (ft->fiber->get_state() == Fiber::TERMINATED || ft->fiber->get_state() == Fiber::EXCEPT) {
+    if (task) {           // 拿到任务
+      if (task->fiber) {  // 是fiber
+        if (task->fiber->get_state() == Fiber::TERMINATED || task->fiber->get_state() == Fiber::EXCEPT) {
           // T或E状态的任务不用处理
           --_active_thread_count;
           continue;
         } else {
-          ft->fiber->enter();  // 开始执行
+          task->fiber->enter();  // 开始执行
           // 执行结束
           --_active_thread_count;
 
-          if (ft->fiber->get_state() == Fiber::READY) {
+          if (task->fiber->get_state() == Fiber::READY) {
             // 用户将状态设置为READY表示希望调度器自动将此任务放入调度队列
-            schedule(ft->fiber);
+            schedule(task->fiber);
           }
         }
 
-      } else if (ft->cb) {  // 是callback
-        auto cb_fiber = std::make_shared<Fiber>(std::move(ft->cb));
+      } else if (task->cb) {  // 是callback
+        auto cb_fiber = std::make_shared<Fiber>(std::move(task->cb));
 
         cb_fiber->enter();
         --_active_thread_count;
